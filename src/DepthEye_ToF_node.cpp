7aa3935ef5eaeebcc8e885e81e697316aceec90d
freitas <pablo.santos1999@gmail.com>
2019-07-10 17:04:29 -0300

    Ready to do the code for the node

---
+++ src/DepthEye_ToF_node.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Int64.h"
+#include <math.h>
+
+
+
+int main(int argc, char **argv)
+{
+	//ros::init(argc, argv,"diff_tf");
+	//Odometry_calc obj;
+	//obj.spin();
+
+
+	return (0);
+}
