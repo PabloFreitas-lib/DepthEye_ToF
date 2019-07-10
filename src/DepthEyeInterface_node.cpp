6986ce1420809fb0dd0539e3e63c6a37f59f44a1
freitas <pablo.santos1999@gmail.com>
2019-07-10 12:28:24 -0300

    Att Cmake File

---
+++ src/DepthEyeInterface_node.cpp
@@ -0,0 +1,16 @@
+
+
+
+/*
+int main(int argc, char *argv[]) {
+ ROS_INFO("Starting bta_tof_driver...");
+ ros::init (argc, argv, "bta_tof_driver");
+ ros::NodeHandle nh, nh_private("~");
+
+ bta_tof_driver::BtaRos camera(nh, nh_private,ros::this_node::getName());
+
+ camera.initialize();
+
+ return 0;
+}
+*/
